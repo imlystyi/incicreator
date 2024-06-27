bb4f56ad10088fcaf14754f03266560d2eb634d9
imlystyi <vladte3c@gmail.com>
2024-06-27 19:54:56 +0300

    build: connect database context to the project as a service

--- InciCreator/Program.cs
+++ InciCreator/Program.cs
@@ -1,4 +1,7 @@
 
+using InciCreator.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace InciCreator
 {
     public class Program
@@ -13,6 +16,11 @@ namespace InciCreator
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
 
             var app = builder.Build();
 
