7a30cc3b275514f5a23abd0ba91206d979b5ac01
imlystyi <vladte3c@gmail.com>
2024-06-27 19:54:08 +0300

    feat: create database context

---
+++ InciCreator/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using InciCreator.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InciCreator.DbContexts;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    public DbSet<Account> Accounts { get; set; }
+
+    public DbSet<Incident> Incidents { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Incident>()
+                    .HasOne(i => i.Account)
+                    .WithMany(a => a.Incidents)
+                    .HasForeignKey(i => i.AccountId);
+
+        modelBuilder.Entity<Account>()
+                    .HasOne(a => a.Contact)
+                    .WithMany(c => c.Accounts)
+                    .HasForeignKey(a => a.ContactId);
+    }
+}
