4a30a1378e76c4219614ca76053ea8bc9fdf925c
imlystyi <vladte3c@gmail.com>
2024-06-27 23:58:36 +0300

    feat: create controller for records creation

---
+++ InciCreator/Controllers/ApplicationController.cs
@@ -0,0 +1,30 @@
+﻿using InciCreator.Exceptions;
+using InciCreator.Models.DataTransferObjects;
+using InciCreator.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InciCreator.Controllers;
+
+[ApiController]
+[Route("api")]
+public class ApplicationController(IApplicationService applicationService) : Controller
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateContact([FromBody] CreationRequest creationRequest)
+    {
+        try
+        {
+            await applicationService.CreateRecords(creationRequest);
+
+            return this.Ok();
+        }
+        catch (AccountNotFoundException e)
+        {
+            return this.NotFound(e.Message);
+        }
+        catch (Exception)
+        {
+            return this.StatusCode(500, "Unexpected error occured.");
+        }
+    }
+}
